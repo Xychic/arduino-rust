382322e045535a3eaf6aa8f0d39e1fdc4165a979
Jacob Turner <jacob11turner@gmail.com>
2022-01-20 14:01:29 +0000

    Remove static mut, replaces with mutex Static mut bad, ha...Or so I have been told.

--- arduino-nano-interrupts/src/main.rs
+++ arduino-nano-interrupts/src/main.rs
@@ -11,20 +11,20 @@ use arduino_hal::{
     },
     prelude::*,
 };
+use avr_device::interrupt::Mutex;
 use panic_halt as _;
 
 use core::{
-    cell, mem,
+    cell::Cell,
+    mem::MaybeUninit,
     sync::atomic::{AtomicBool, Ordering},
 };
 
 static TMR_OVERFLOW: AtomicBool = AtomicBool::new(false);
 static ROTARY_CHANGE: AtomicBool = AtomicBool::new(false);
-static VAL: avr_device::interrupt::Mutex<cell::Cell<u16>> =
-    avr_device::interrupt::Mutex::new(cell::Cell::new(0));
-
-static mut ROTARY_PINS: mem::MaybeUninit<[Pin<Input<PullUp>, Dynamic>; 2]> =
-    mem::MaybeUninit::uninit();
+static VAL: Mutex<Cell<u16>> = Mutex::new(Cell::new(0));
+static ROTARY_PINS: Mutex<Cell<MaybeUninit<[Pin<Input<PullUp>, Dynamic>; 2]>>> =
+    Mutex::new(Cell::new(MaybeUninit::uninit()));
 
 #[arduino_hal::entry]
 fn main() -> ! {
@@ -32,12 +32,13 @@ fn main() -> ! {
     let pins = arduino_hal::pins!(peripherals);
     let mut serial = arduino_hal::default_serial!(peripherals, pins, 9600);
 
-    unsafe {
-        ROTARY_PINS = mem::MaybeUninit::new([
-            pins.d8.into_pull_up_input().downgrade(),
-            pins.d2.into_pull_up_input().downgrade(),
-        ]);
-    }
+    let rotary_pins = [ 
+        pins.d8.into_pull_up_input().downgrade(),
+        pins.d2.into_pull_up_input().downgrade(),
+    ];
+    avr_device::interrupt::free(|cs| {
+        ROTARY_PINS.borrow(cs).set(MaybeUninit::new(rotary_pins));
+    });
 
     let timer1 = peripherals.TC1;
     timer1.tccr1a.write(|w| unsafe { w.bits(0) });
@@ -74,14 +75,11 @@ fn TIMER1_COMPA() {
 
 #[avr_device::interrupt(atmega328p)]
 #[allow(non_snake_case)]
-unsafe fn PCINT2() {
-    let rotary_pins = &*ROTARY_PINS.as_ptr();
-    // Detect Rising edge
-    if rotary_pins[1].is_low() {
-        // Store the fact that a change has been detected
-        ROTARY_CHANGE.store(true, Ordering::SeqCst);
-        avr_device::interrupt::free(|cs| {
-            // Get the current value from the atomic
+fn PCINT2() {
+    avr_device::interrupt::free(|cs| {
+        let rotary_pins = unsafe { &*(&*ROTARY_PINS.borrow(cs).as_ptr()).as_ptr() };
+        if rotary_pins[1].is_low() {
+            ROTARY_CHANGE.store(true, Ordering::SeqCst);
             let val_cell = VAL.borrow(cs);
             let val = val_cell.get();
             if rotary_pins[0].is_low() && val < u16::MAX {
@@ -89,14 +87,11 @@ unsafe fn PCINT2() {
             } else if rotary_pins[0].is_high() && val > 0 {
                 val_cell.set(val - 1);
             }
-        });
-    }
+        }
+    });
 }
 
-fn get_from_mutex<T>(mutex: &avr_device::interrupt::Mutex<cell::Cell<T>>) -> T
-where
-    T: Copy,
-{
+fn get_from_mutex<T: Copy>(mutex: &Mutex<Cell<T>>) -> T {
     avr_device::interrupt::free(|cs| mutex.borrow(cs).get())
 }
 
